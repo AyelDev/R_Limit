926d12ccaf7433733e66f915d26c10f1c94f1f76
Ariel <ariabelgas@proweaver.net>
2026-06-22 16:43:19 +0800

    added global function and variables

--- src/main/java/com/ayeldev/ratelimiter/controller/TestController.java
+++ src/main/java/com/ayeldev/ratelimiter/controller/TestController.java
@@ -6,34 +6,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpServerErrorException;
+
 import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 @RestController
 @RequestMapping("/api")
 public class TestController {
 
     private final Gson gson;
+    private final String jsonContent = "application/json";
+    private final String charEncoding = "UTF-8";
 
     @Autowired
-    public TestController(Gson gson){
+    public TestController(Gson gson) {
         this.gson = gson;
     }
 
+    public void setResponse(HttpServletResponse response, int status) {
+        response.setStatus(status);
+        response.setContentType(jsonContent);
+        response.setCharacterEncoding(charEncoding);
+    }
+
     @GetMapping("/test")
-    public String testRateLimit(HttpServletResponse response){
-            response.setStatus(HttpServletResponse.SC_OK);
-            response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");
+    public String testRateLimit(HttpServletResponse response) {
+        this.setResponse(response, 200);
         return this.gson.toJson(Map.of("message", "Request reached successfully"));
     }
 
     @GetMapping("/sliding-windows")
-    public String slidingWindows(HttpServletResponse response) {
-        return new String();
-    }
-    
+    // public String slidingWindows(HttpServletResponse response) {
+    //     // this.setResponse(response);
+    // }
+
 }
