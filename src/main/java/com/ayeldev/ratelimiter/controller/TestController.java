2e3b9ca84254712864ab309b0cf71863aea6c26f
Ariel <ariabelgas@proweaver.net>
2026-06-22 16:30:09 +0800

    added sliding-windows api route

--- src/main/java/com/ayeldev/ratelimiter/controller/TestController.java
+++ src/main/java/com/ayeldev/ratelimiter/controller/TestController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -28,4 +30,10 @@ public class TestController {
             response.setCharacterEncoding("UTF-8");
         return this.gson.toJson(Map.of("message", "Request reached successfully"));
     }
+
+    @GetMapping("/sliding-windows")
+    public String slidingWindows(HttpServletResponse response) {
+        return new String();
+    }
+    
 }
